a11ea37087e7b2df0d58f487784dc0bd59d1a3f3
MadsKrist <madskris@proton.me>
2024-12-19 15:06:29 +0100

    Init

--- src/belial_backend/__init__.py
+++ src/belial_backend/__init__.py
@@ -1,3 +1,12 @@
-# SPDX-FileCopyrightText: 2024-present ManMade2 <madskris@proton.me>
-#
-# SPDX-License-Identifier: MIT
+from flask import Flask
+from .api import assets_blueprint
+from flask_cors import CORS
+
+
+def create_app():
+    app = Flask(__name__)
+    CORS(app)
+
+    app.register_blueprint(assets_blueprint, url_prefix="/api/v1")
+
+    return app
